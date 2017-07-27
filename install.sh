3589b24051a0f3e2f4199d67a4f99ee880ff0b5f
EasonChen <aihoom@126.com>
2017-07-27 15:30:50 +0800

    修改不能查询流量的问题
    修改不能查询流量的问题

--- install.sh
+++ install.sh
@@ -147,6 +147,7 @@ sed -i "s/sspanelv2/mudbjson/g" /usr/local/shadowsocksr/userapiconfig.py
 sed -i "s/UPDATE_TIME = 60/UPDATE_TIME = 10/g" /usr/local/shadowsocksr/userapiconfig.py
 sed -i "s/SERVER_PUB_ADDR = '127.0.0.1'/SERVER_PUB_ADDR = '$(wget -qO- -t1 -T2 ipinfo.io/ip)'/" /usr/local/shadowsocksr/userapiconfig.py
 #INstall Success
+chmod +x -R /usr/local/SSR-Bash-Python/www/cgi-bin
 bash /usr/local/SSR-Bash-Python/self-check.sh
 echo '安装完成！输入 ssr 即可使用本程序~'
 echo 'Telegram Group: https://t.me/functionclub'
